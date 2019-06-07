818180736821554e78db9bb6f94cb0020d86481e
Sebastian <sebasipi501@gmail.com>
2019-06-07 13:41:28 -0500

    Problemas
    PowerShells

---
+++ Data/HappyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Happy.Models;
+
+    public class HappyContext : DbContext
+    {
+        public HappyContext (DbContextOptions<HappyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Happy.Models.Categoria> Categoria { get; set; }
+    }
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace Happy
 {
@@ -33,6 +34,9 @@ namespace Happy
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<HappyContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("HappyContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
